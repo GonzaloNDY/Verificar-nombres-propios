a6b666c23eaf269112c87cc33fc0ad8d279062e6
Gonzalo <gonzalio100@gmail.com>
2023-09-01 15:54:32 -0300

    agregado del código y el archivo readme con la descripción detallada

---
+++ código.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX 200
+
+int esNombreValido(char nombre[]){
+    if (strspn(nombre, "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM ") && contarPalabras(nombre) <= 3){  // Se aceptarán como válidos solo hasta 3 nombres y 3 apellidos
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int contarPalabras(char cadena[]){
+    int cantidad = 0;
+
+    if (isalpha(cadena[0])){
+        ++cantidad;
+    }
+
+    for (int i = 1; cadena[i] != '\0'; i++){
+        if (!isalpha(cadena[i - 1]) && isalpha(cadena[i]))
+            ++cantidad;
+    }
+
+    return cantidad;
+}
